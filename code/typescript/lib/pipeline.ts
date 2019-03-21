3a28999c5add65077f803e91eb84f706105f21db
Elad Ben-Israel <benisrae@amazon.com>
2019-03-21 13:56:49 +0200

    add stack2

--- code/typescript/lib/pipeline.ts
+++ code/typescript/lib/pipeline.ts
@@ -2,6 +2,7 @@ import codepipeline = require('@aws-cdk/aws-codepipeline');
 import { App, Stack, StackProps } from '@aws-cdk/cdk';
 import { BootstrapPipelineSource, DeployStackAction } from './bootstrap';
 import { CdkWorkshopStack } from './workshop-stack';
+import sns = require('@aws-cdk/aws-sns');
 
 export interface WorkshopPipelineProps extends StackProps {
   workshopStack: CdkWorkshopStack
@@ -21,11 +22,25 @@ export class WorkshopPipeline extends Stack {
       admin: true
     });
 
+    const deployStack2 = new DeployStackAction(this, 'DeployRandomStack', {
+      source,
+      stack: new RandomStack(app, 'RandomStack'),
+      admin: true
+    });
+
     new codepipeline.Pipeline(this, 'Pipeline', {
       stages: [
         { name: 'Source', actions: [ source ] },
-        { name: 'DeployWorkshop', actions: [ deploy ] }
+        { name: 'DeployWorkshop', actions: [ deploy, deployStack2 ] }
       ]
     });
+  }
+}
+
+class RandomStack extends Stack {
+  constructor(app: App, id: string) {
+    super(app, id);
+
+    new sns.Topic(this, 'MyTopic');
   }
 }
